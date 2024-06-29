1163a74046ab129af3eb5aed87ea068c4163b6a9
Lucas_M54 <lucasm54800@gmail.com>
2024-06-29 02:48:32 +0200

    add cancel button

--- Applications Automatical Installer copy.py
+++ Applications Automatical Installer copy.py
@@ -17,20 +17,6 @@ import webbrowser
 logging.basicConfig(filename='app_installer.log', level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-def store_url(productid):
-    return f"ms-windows-store://pdp?hl=fr-fr&gl=fr&referrer=storeforweb&source=https%3A%2F%2Fwww.google.com%2F&productid={productid}&mode=mini&pos=7%2C2%2C1922%2C922"
-
-def extension_url(id):
-    return f"https://chromewebstore.google.com/detail/{id}"
-
-def get_url(app_details):
-    if 'type' in app_details:
-        if app_details['type'] == 'microsoft':
-            return store_url(app_details['productid'])
-        elif app_details['type'] == 'extension':
-            return extension_url(app_details['id'])
-    return app_details['url']
-
 class DownloadThread(QThread):
     progress_signal = pyqtSignal(int)
     finished_signal = pyqtSignal(str)
@@ -39,20 +25,15 @@ class DownloadThread(QThread):
         QThread.__init__(self)
         self.url = url
         self.file_path = file_path
-        self.is_cancelled = False
 
     def run(self):
         try:
             response = requests.get(self.url, stream=True)
-            response.raise_for_status()
+            response.raise_for_status()  # This will raise an exception for HTTP errors
             total_size = int(response.headers.get('content-length', 0))
             block_size = 1024
             with open(self.file_path, "wb") as f:
                 for data in response.iter_content(block_size):
-                    if self.is_cancelled:
-                        f.close()
-                        os.remove(self.file_path)
-                        return
                     f.write(data)
                     if total_size:
                         self.progress_signal.emit(f.tell() * 100 // total_size)
@@ -61,30 +42,26 @@ class DownloadThread(QThread):
             logging.error(f"Download failed: {str(e)}")
             self.finished_signal.emit("")
 
-    def cancel(self):
-        self.is_cancelled = True
-
 class InstallationManager(QObject):
     installation_complete = pyqtSignal()
-    installation_started = pyqtSignal(str)
+    installation_started = pyqtSignal()
     installation_finished = pyqtSignal()
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.queue = []
         self.current_thread = None
-        self.current_app = ""
 
     def add_to_queue(self, app, details, downloads_path):
         self.queue.append((app, details, downloads_path))
         if not self.current_thread:
+            self.installation_started.emit()
             self.process_next()
 
     def process_next(self):
         if self.queue:
             app, details, downloads_path = self.queue.pop(0)
-            self.current_app = app
-            url = get_url(details)
+            url = details["url"]
             app_type = details.get("type", "")
 
             if app_type in ['extension', 'microsoft'] or "(manual)" in app:
@@ -96,13 +73,10 @@ class InstallationManager(QObject):
                 self.current_thread = DownloadThread(url, str(file_path))
                 self.current_thread.progress_signal.connect(self.parent().update_progress)
                 self.current_thread.finished_signal.connect(lambda fp, a=app: self.install_application(fp, a))
-                self.installation_started.emit(app)
                 self.current_thread.start()
         else:
             self.installation_finished.emit()
             self.installation_complete.emit()
-
-        self.parent().update_button_states()
 
     def install_application(self, file_path, app):
         if file_path:
@@ -116,15 +90,21 @@ class InstallationManager(QObject):
             QMessageBox.critical(None, "Error", f"Failed to download {app}")
         
         self.current_thread = None
-        self.current_app = ""
         self.process_next()
 
-    def cancel_current_installation(self):
-        if self.current_thread:
-            self.current_thread.cancel()
-            self.current_thread = None
-            self.current_app = ""
-            self.process_next()
+def store_url(productid):
+    return f"ms-windows-store://pdp?hl=fr-fr&gl=fr&referrer=storeforweb&source=https%3A%2F%2Fwww.google.com%2F&productid={productid}&mode=mini&pos=7%2C2%2C1922%2C922"
+
+def extension_url(id):
+    return f"https://chromewebstore.google.com/detail/{id}"
+
+def get_url(app_details):
+    if 'type' in app_details:
+        if app_details['type'] == 'microsoft':
+            return store_url(app_details['productid'])
+        elif app_details['type'] == 'extension':
+            return extension_url(app_details['id'])
+    return app_details['url']
 
 class AppInstaller(QWidget):
     def __init__(self):
@@ -142,6 +122,7 @@ class AppInstaller(QWidget):
         try:
             with open('applications.json', 'r') as f:
                 self.applications = json.load(f)
+            # Generate URLs for Microsoft Store apps and Chrome extensions
             for app, details in self.applications.items():
                 if 'type' in details:
                     details['url'] = get_url(details)
@@ -150,6 +131,10 @@ class AppInstaller(QWidget):
             self.applications = self.get_default_applications()
 
     def get_default_applications(self):
+        def Store_url(productid):
+            return f"ms-windows-store://pdp?hl=fr-fr&gl=fr&referrer=storeforweb&source=https%3A%2F%2Fwww.google.com%2F&productid={productid}&mode=mini&pos=7%2C2%2C1922%2C922"
+        def extension_url(id):
+            return f"https://chromewebstore.google.com/detail/{id}"
         return {
             "Brave": {"url": "https://laptop-updates.brave.com/latest/winx64"},
             "Discord": {"url": "https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x64"},
@@ -165,8 +150,8 @@ class AppInstaller(QWidget):
             "Hower Zoom+": {"url": extension_url("hover-zoom%2B/pccckmaobkjjboncdfnnofkonhgpceea"), "type": "extension"},
             "Return YouTube Dislike": {"url": extension_url("return-youtube-dislike/gebbhagfogifgggkldgodflihgfeippi"), "type": "extension"},
             "SponsorBlock": {"url": extension_url("sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone"), "type": "extension"},
-            "Visual Studio Code": {"url": store_url("xp9khm4bk9fz7q"), "type": "microsoft"},
-            "Office": {"url": store_url("9wzdncrd29v9"), "type": "microsoft"},
+            "Visual Studio Code": {"url": Store_url("xp9khm4bk9fz7q"), "type": "microsoft"},
+            "Office": {"url": Store_url("9wzdncrd29v9"), "type": "microsoft"},
         }
 
     def setup_ui(self):
@@ -226,24 +211,15 @@ class AppInstaller(QWidget):
         button_layout = QHBoxLayout()
         layout.addLayout(button_layout)
 
-        self.install_button = QPushButton("Install")
-        self.install_button.clicked.connect(self.install_informations)
-        
-        self.cancel_button = QPushButton("Cancel")
-        self.cancel_button.clicked.connect(self.cancel_installation)
-        self.cancel_button.setEnabled(False)
-
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(self.install_button)
-        button_layout.addWidget(self.cancel_button)
-        layout.addLayout(button_layout)
+        self.create_and_add_button("Install", self.install_applications, button_layout)
+        self.create_and_add_button("Quit", self.close, button_layout)
 
         self.progress_bar = QProgressBar()
         layout.addWidget(self.progress_bar)
 
-        self.current_app_label = QLabel()
-        layout.addWidget(self.current_app_label)
-
+        self.install_button = QPushButton("Install")
+        self.install_button.clicked.connect(self.install_applications)
+    
     def create_select_all_button(self, column_index):
         button = QPushButton("Select All")
         button.clicked.connect(lambda: self.select_all_column(self.column_checkboxes[column_index]))
@@ -278,11 +254,7 @@ class AppInstaller(QWidget):
     def get_application_type(self, app_details):
         return app_details.get("type", "")
 
-    def install_informations(self):
-        if self.installation_manager.current_thread:
-            QMessageBox.warning(self, "Warning", "An installation is already in progress. Please wait for it to finish.")
-            return
-
+    def install_applications(self):
         applications_to_install = [app for app, checkbox in self.checkboxes.items() if checkbox.isChecked()]
         if not applications_to_install:
             QMessageBox.information(self, "Information", "No application selected!")
@@ -292,31 +264,21 @@ class AppInstaller(QWidget):
         for app in applications_to_install:
             details = self.applications[app]
             self.installation_manager.add_to_queue(app, details, downloads_path)
-        self.update_button_states()
 
     def update_progress(self, value):
         self.progress_bar.setValue(value)
 
-    def update_button_states(self):
-        has_queue = bool(self.installation_manager.queue)
-        self.install_button.setEnabled(not has_queue)
-        self.cancel_button.setEnabled(has_queue)
-
-    def cancel_installation(self):
-        self.installation_manager.cancel_current_installation()
-        self.progress_bar.setValue(0)
-
     def on_installation_complete(self):
         self.close()
 
-    def on_installation_started(self, app):
-        self.update_button_states()
-        self.current_app_label.setText(f"Installing: {app}")
+    def on_installation_started(self):
+        self.install_button.setEnabled(False)
+        self.install_button.setText("Installation in progress...")
 
     def on_installation_finished(self):
-        self.update_button_states()
+        self.install_button.setEnabled(True)
+        self.install_button.setText("Install")
         self.progress_bar.setValue(0)
-        self.current_app_label.setText("")
 
     def get_downloads_path(self):
         sub_key = r"Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
