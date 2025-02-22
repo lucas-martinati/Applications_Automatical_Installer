c96224ca6e960abe4dc47945d1619912581967df
Lucas_M54 <lucasm54800@gmail.com>
2025-02-22 16:21:27 +0100

    ez

--- Applications_Automatical_Installer.py
+++ Applications_Automatical_Installer.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
                              QCheckBox, QPushButton, QMessageBox, QLabel,
                              QSpacerItem, QSizePolicy, QScrollArea, QProgressBar, )
-from PyQt5.QtGui import QFont, QPainter, QPen
+from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, QObject
 import requests
 import winreg
